a2829766b8242b666b9e38dcf59e17c7b1e4c884
domains18 <gibsonsgibson88@gmail.com>
2022-12-29 16:41:57 +0300

    database handler

--- backend/Conf/Database.js
+++ backend/Conf/Database.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', false);
 const connectDatabase = async() =>{
     try{
         const conn = await mongoose.connect(process.env.DATABASE_URL);
-        console.log(`Succesfully Authenticated Database at:${conn.connection.host}`.green)
+        console.log(`Succesfully Authenticated Database:${conn.connection.host}`)
     } catch (error){
         console.log(error)
         process.exit(1)
