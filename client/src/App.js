1017c17a96ff576750cb8c9e6e0ffd76159b709c
Domains18 <gibsonsgibson88@gmail.com>
2022-12-31 17:49:06 +0300

    signup-login forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import Login from './pages/Login';
 import SignUp from './pages/signUp';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 import './App.css';
 
 function App() {
   return (
-  <div>
-    <Login/>
-    <SignUp/>
-  </div>
+    <Router>
+      <Routes>
+        <Route path='/'element={<Dashboard/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/signup' element={<SignUp/>} />
+      </Routes>
+    </Router>
   );
 }
 
