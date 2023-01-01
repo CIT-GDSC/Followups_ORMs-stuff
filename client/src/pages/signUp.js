cc4a207e29d2b67efe86538b91487e4b51557c38
Domains18 <gibsonsgibson88@gmail.com>
2023-01-01 17:06:45 +0300

    signup js

--- client/src/pages/signUp.js
+++ client/src/pages/signUp.js
@@ -2,7 +2,26 @@ import React from 'react'
 
 function SignUp() {
 	return (
-		
+		<section className="login">
+            <div className="background">
+                <div className="shape"></div>
+                <div className="shape"></div>
+            </div>
+            <form>
+                <h3>Welcome, SignUp here</h3>
+                <label htmlFor="name"></label>
+				<input className="input" type="text" name="" id="" placeholder="Name" />
+                <input className=" input" type="text" name="" id="" placeholder="Email" />
+                <label htmlFor="password"></label>
+                <input className="input" type="text" placeholder="password" />
+                <input className="input" type="text" placeholder="Repeat password" />
+                <input type="submit" value="Login" className="button" />
+                <div className="social">
+                    <div className="go"><i className="fab fab-google">Google</i></div>
+
+                </div>
+            </form>
+        </section>
 	)
 }
 
